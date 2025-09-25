e51645d4360c62945d05983b1a3b5fea2a5f4d10
Karen Lu <karenlukn@gmail.com>
2025-09-24 21:44:46 -0400

    style dashboard

---
+++ app/(Kambaz)/Courses/CourseCard.tsx
@@ -0,0 +1,22 @@
+import { Card, CardBody, CardImg, CardText, CardTitle, Row, Col } from "react-bootstrap";
+import Link from "next/link";
+
+export default function CourseCard({ course }: { course: any }) {
+    return (
+
+        <Col className="wd-dashboard-course" style={{ width: "270px" }}>
+            <Card>
+                <Link href={`/Courses/${course.code}/Home`}
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                    <CardImg variant="top" src={course.image} width="100%" height={160} />
+                    <CardBody>
+                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">{course.code} {course.title}</CardTitle>
+                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                            {course.description}
+                        </CardText>
+                    </CardBody>
+                </Link>
+            </Card>
+        </Col>
+    );
+}
--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,97 +1,31 @@
-import Link from "next/link";
-import Image from "next/image";
+import CourseCard from "../Courses/CourseCard";
+import { Row } from "react-bootstrap";
+
 export default function Dashboard() {
+
+  const courses = [
+    { code: "MR5010", title: "Distribution of Dolphins", description: "This course explores the various species of dolphins, their habitats, behaviors, and conservation efforts.", image: "/images/dolphin.jpg" },
+    { code: "MR5020", title: "Biology of Pufferfishes", description: "This course delves into the unique biological characteristics of pufferfishes, including their defense mechanisms and ecological roles.", image: "/images/pufferfish.jpg" },
+    { code: "MR5030", title: "Anatomy of Seals", description: "This course examines the anatomical features of seals, focusing on their adaptations to marine environments.", image: "/images/seal.jpg" },
+    { code: "MR5040", title: "Evolution of Sharks", description: "This course traces the evolutionary history of sharks, exploring their diversity and survival strategies over millions of years.", image: "/images/shark.jpg" },
+    { code: "MR5050", title: "Ecology of Starfishes", description: "This course investigates the ecological roles of starfishes in marine ecosystems, including their interactions with other species.", image: "/images/starfish.jpg" },
+    { code: "MR5060", title: "Diversity of Stingrays", description: "This course explores the diversity of stingrays, their habitats, behaviors, and the challenges they face in the wild.", image: "/images/stingray.jpg" },
+    { code: "MR5070", title: "Diet of Sea Turtles", description: "This course examines the dietary habits of sea turtles, their foraging behaviors, and nutritional needs.", image: "/images/turtle.jpg" }
+  ]
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/dolphin.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR501</h5>
-              <p className="wd-dashboard-course-title">
-                Distribution of Dolphins {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
+      <main className="px-4 py-2">
+        <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+        <div id="wd-dashboard-courses">
+          <Row xs={1} md={5} className="g-4 width-100">
+            {courses.map((course) => (
+              <CourseCard key={course.code} course={course} />
+            ))}
+          </Row>
         </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/pufferfish.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR502</h5>
-              <p className="wd-dashboard-course-title">
-                Biology of Pufferfishes {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/seal.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR503</h5>
-              <p className="wd-dashboard-course-title">
-                Anatomy of Seals {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/shark.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR504</h5>
-              <p className="wd-dashboard-course-title">
-                Evolution of Sharks {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/starfish.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR505</h5>
-              <p className="wd-dashboard-course-title">
-                Ecology of Starfishes {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/stingray.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR506</h5>
-              <p className="wd-dashboard-course-title">
-                Diversity of Stingrays {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/turtle.jpg" alt="react" width={200} height={150} />
-            <div>
-              <h5> MR507</h5>
-              <p className="wd-dashboard-course-title">
-                Diet of Sea Turtles {" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-
-      </div>
-    </div>
+      </main>
+    </div >
   );
 }
