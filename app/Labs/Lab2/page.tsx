7a0f703621e4609c48e41eff0859f9a93ee75d1e
Karen Lu <karenlukn@gmail.com>
2025-09-28 13:07:29 -0400

    Escape ' and "

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import "./index.css";
 import ForeGroundColors from "./ForeGroundColors";
@@ -15,7 +15,7 @@ import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
 
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
 import BootstrapGrids from "./BootstrapGrids";
 import ScreenSizeLabel from "./ScreenSizeLabel";
@@ -32,7 +32,7 @@ export default function Lab2() {
         <h3>Styling with the STYLE attribute</h3>
         <p>
           Style attribute allows configuring look and feel
-          right on the element. Although it's very convenient
+          right on the element. Although it&apos;s very convenient
           it is considered bad practice and you should avoid
           using the style attribute
         </p>
@@ -45,7 +45,7 @@ export default function Lab2() {
             elements of the same name, e.g., P, we can refer to a specific element by its ID
           </p>
           <p id="wd-id-selector-2">
-            Here's another paragraph using a different ID and a different look and
+            Here&apos;s another paragraph using a different ID and a different look and
             feel
           </p>
         </div>
@@ -71,7 +71,7 @@ export default function Lab2() {
               Selectors can be combined to refer elements in particular
               places in the document
               <p className="wd-selector-3">
-                This paragraph's red background is referenced as
+                This paragraph&apos;s red background is referenced as
                 <br />
                 .selector-2 .selector3<br />
                 meaning the descendant of some ancestor.<br />
