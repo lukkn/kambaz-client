36284b3b70593ef79707f5df8978eb5bbcde9485
Karen Lu <karenlukn@gmail.com>
2025-10-01 15:04:55 -0400

    update lab2 to rubric

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -5,25 +5,26 @@
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
 export default function TOC() {
- return (
-   <Nav variant="pills">
-     <NavItem>
-       <NavLink id="wd-labs-link" href="/Labs" as={Link}>Labs</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink id="wd-lab1-link" href="/Labs/Lab1" as={Link}>Lab 1</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink id="wd-lab-link" href="/Labs/Lab2" as={Link}>Lab 2</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink id="wd-lab3-link" href="/Labs/Lab3" as={Link}>Lab 3</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink id="wd-kambaz-link" href="/" as={Link}>Kambaz</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink id="wd-github" href="https://github.com/lukkn/kambaz-cs5610">GitHub</NavLink>
-     </NavItem>
-   </Nav>
-);}
+  return (
+    <Nav variant="pills">
+      <NavItem>
+        <NavLink href="/Labs" as={Link}>Labs</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab1" as={Link}>Lab 1</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab2" as={Link}>Lab 2</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab3" as={Link}>Lab 3</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/" as={Link}>Kambaz</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/lukkn/kambaz-cs5610">My GitHub</NavLink>
+      </NavItem>
+    </Nav>
+  );
+}
