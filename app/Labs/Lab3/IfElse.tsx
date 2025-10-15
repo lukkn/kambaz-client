d4de9c25bc779b94f2cf033ed7413d596c87fc3c
Karen Lu <karenlukn@gmail.com>
2025-10-15 17:11:59 -0400

    Revert "vercel deployment checks"
    This reverts commit 84ed416a4a3aed97c0b4e983be52d92bf77c351a.

--- app/Labs/Lab3/IfElse.tsx
+++ app/Labs/Lab3/IfElse.tsx
@@ -1,5 +1,5 @@
 export default function IfElse() {
-    const true1 = true, false1 = false;
+    let true1 = true, false1 = false;
     return (
         <div id="wd-if-else">
             <h4>If Else</h4>
