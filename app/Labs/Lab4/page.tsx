203e7a19ff90e0aa1b5c74c3e22fb85abd615da5
Karen Lu <70594133+lukkn@users.noreply.github.com>
2025-10-28 17:38:50 -0400

    lab4 pt4.1 and 4.2

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,8 +1,42 @@
+"use client";
+
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariables";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+
 export default function Lab4() {
+    function sayHello() {
+        alert("Hello");
+    }
     return (
         <div>
-            <h1>Lab 4</h1>
-            <p>This is the content for Lab 4.</p>
+            <h2>Lab 4</h2>
+            {/* Handling Click Events */}
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+
+            {/* Managing Compoenent State */}
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
+
+            {/* Managing Application State with Redux */}
+
+            
         </div>
     );
 }
