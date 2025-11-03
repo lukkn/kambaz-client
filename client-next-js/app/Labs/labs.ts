8e527e4886c2a4a95872214f12c4c2d78a8a2c04
Karen Lu <70594133+lukkn@users.noreply.github.com>
2025-11-03 11:30:07 -0500

    refactor

--- client-next-js/app/Labs/labs.ts
+++ client-next-js/app/Labs/labs.ts
@@ -2,7 +2,8 @@ const labs = [
     "HTML Examples",
     "CSS Basics",
     "Javascript Fundamentals",
-    "Maintaining State in React Applications"
+    "Maintaining State in React Applications",
+    "Implementing RESTful Web APIs with Express.js"
 ]
 
 export default labs;
